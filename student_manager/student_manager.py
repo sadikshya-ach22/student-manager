1bf2d5397f666fe1852b5069a5b64681610facd3
Sadikshya Acharya <sadikshyaacharya16@gmail.com>
2026-05-16 20:43:43 -0500

    Feat: Implemented resource list view, computed properties, and delete functionality

--- student_manager/student_manager.py
+++ student_manager/student_manager.py
@@ -9,8 +9,10 @@ class Category(rx.Model):
     name: str
     items: list[Resource] = []   #Category owns a list of resources
 
+#-------------------------------------------------------------------------------------------------
+#Defining the Brain of the project
+#-------------------------------------------------------------------------------------------------
 
-#---Defining the Brain of the project---
 class State(rx.State):
     categories: list[Category] = [] #1. The List to add categories.
     new_category_name: str = ""  #2. The input:stores currently typed category name
@@ -18,7 +20,7 @@ class State(rx.State):
     new_resource_url: str = ""
 
     #Pointer to remember which category is currently open
-    selected_category: Category = Category(name= "")
+    selected_category_name: str = ""
 
     #Logic for Categories
     def set_new_category_name(self, name: str):
@@ -34,11 +36,20 @@ class State(rx.State):
     
     #The opener: this function sets "Pointer" to the card user clicked.
     def select_category(self, cat: Category):
-        self.selected_category = cat
+        self.selected_category_name = cat.name
 
     #The closer: resets the pointer to go back to the main dashboard
     def clear_selection(self):
-        self.selected_category = Category(name= "")
+        self.selected_category_name = ""       #reset the pointer to an empty string
+    
+    #COMPUTED PROPERTY
+    @rx.var
+    def current_items(self) -> list[Resource]:
+        #Provides a clean, guaranteed list of resources for UI to loop over
+        for cat in self.categories:
+            if cat.name == self.selected_category_name:
+                return cat.items
+        return [] #returning empty list if nothing matches
 
     #Logic for Resources
     def add_resource(self):
@@ -48,13 +59,21 @@ class State(rx.State):
         )
 
         for cat in self.categories:          #pushing the resource into the selected category
-            if cat.name == self.selected_category.name:
+            if cat.name == self.selected_category_name:
                 cat.items.append(new_res)
                 break
 
         self.new_resource_title = ""          #Cleaning up the input boxes
         self.new_resource_url = ""
-        self.selected_category = self.selected_category    #Refresh the UI
+        self.categories = self.categories
+
+    #Deleting the resource
+    def delete_resource(self, resource_to_delete: Resource):
+        for cat in self.categories:
+            if cat.name == self.selected_category_name:
+                cat.items.remove(resource_to_delete)
+                break
+        self.categories = self.categories
 
     def set_new_resource_title(self, title: str):
         self.new_resource_title = title
@@ -63,8 +82,9 @@ class State(rx.State):
         self.new_resource_url = url
 
     
-
-# ---Defining the body of the app (UI)---
+#-------------------------------------------------------------------------------------------------
+# Defining the body of the app (UI)
+#-------------------------------------------------------------------------------------------------
 
 def index()-> rx.Component:
     return rx.container(
@@ -73,11 +93,11 @@ def index()-> rx.Component:
 
             #Using rx.cond to decide what to show
             rx.cond(
-                State.selected_category.name != "",
+                State.selected_category_name != "",
                 #If true: show the detail view
                 rx.vstack(
                     rx.button("<-Back to Dashboard", on_click = State.clear_selection),
-                    rx.heading(State.selected_category.name, size="7"),
+                    rx.heading(State.selected_category_name, size="7"),
                     #THE RESOURCE FORM
                     rx.vstack(
                         rx.input(
@@ -99,9 +119,46 @@ def index()-> rx.Component:
                         border_radius= "lg",
                         width = "400px",
                     ),
+                    
+
+                   #THE RESOURCE LIST VIEW
+                    rx.vstack(
+                        rx.heading("Saved Resources", size = "4"),
+                        rx.foreach(
+                            State.current_items,
+                            lambda res:rx.hstack(
+                                rx.text(res.title, weight = "medium"),
+
+                                rx.hstack(
+                                    rx.link(
+                                    "Open Link->",
+                                    href= res.url,
+                                    is_external = True,
+                                    color_scheme = 'blue'
+                                    ),
+                                    rx.button(
+                                        "X",
+                                        color_scheme= "red",
+                                        size= "1",
+                                        variant = "soft",
+                                        on_click= lambda: State.delete_resource(res)
+                                    ),
+                                    spacing= "3"
+                                ),
+                                width = '100%',
+                                justify = "between",
+                                padding = "2",
+                                background_color = rx.color("gray",2),
+                                border_radius = "md",
+                            )
+                        ),
+                        width = "400px",
+                        align= "stretch",
+                        spacing = "2",
+                    ),
                     align= "start",
-                    width = "100%",
-                    spacing="4",
+                    width="100%",
+                    spacing="6",
                 ),
 
                 #If false: Show the main input and grid
